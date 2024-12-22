c512f9660709d6efea2dd3abb4b7d0495734891c
papaidas9091 <officialpapaidas@gmail.com>
2024-12-22 17:43:49 +0530

    code

--- Inventory/obj/Debug/net8.0/Inventory.AssemblyInfo.cs
+++ Inventory/obj/Debug/net8.0/Inventory.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4f1f99523631153aad9c9247cd3430dcf0797ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8612cdb5110d5de5a47de0474129b24d280d491")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
